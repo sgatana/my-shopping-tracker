74e2a319129d7c061f2b9ae0f85bd2bc77edb6c2
gatana <sgatana@gmail.com>
2017-11-17 17:15:16 +0300

    Update test_config file to fix failing tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,13 +11,12 @@ class TestConfig(unittest.TestCase):
 
     def test_development_config(self):
         self.assertTrue(self.development.DEBUG)
-        self.assertEqual(self.development.SQLALCHEMY_DATABASE_URI, "postgresql://postgres:steve012@localhost/flask_api")
         self.assertTrue(self.development.SQLALCHEMY_ECHO)
-        self.assertFalse(self.development.SECRET_KEY is'hello')
+        self.assertFalse(self.development.SECRET_KEY is'this is my secret key')
 
     def test_testing_config(self):
         self.assertFalse(self.testing.DEBUG)
-        self.assertTrue(self.testing.SQLALCHEMY_DATABASE_URI == "postgresql://postgres:steve012@localhost/test_db")
+        self.assertTrue(self.testing.SQLALCHEMY_DATABASE_URI == os.environ.get('TESTDB'))
         self.assertTrue(self.testing.TESTING)
 
     def test_production_config(self):
