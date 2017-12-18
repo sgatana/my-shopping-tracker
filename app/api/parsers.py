5791df4fec94b8ca3f52f3f7467a505068de45b3
gatana <sgatana@gmail.com>
2017-12-18 17:11:22 +0300

    [Bug] Remove unused code

--- app/api/parsers.py
+++ app/api/parsers.py
@@ -1,15 +1,5 @@
 from flask_restplus import reqparse
 
-item_parser = reqparse.RequestParser()
-item_parser.add_argument('name', type=str, required=True,
-                         help='Item Name')
-item_parser.add_argument('price', required=True,
-                         help='Item Price should not be a string')
-item_parser.add_argument('quantity', required=True,
-                         help='Item Quantity should not be a string')
-item_parser.add_argument('shoppinglist_id', type=int, required=True,
-                         help='Shoppinglist id is Required')
-
 update_shoppinglist_parser = reqparse.RequestParser()
 update_shoppinglist_parser.add_argument('name', type=str,
                                         help='New Shoppinglist Name')
@@ -20,13 +10,4 @@ update_item_parser = reqparse.RequestParser()
 update_item_parser.add_argument('name', type=str, required=True, help='Item name should be provided')
 update_item_parser.add_argument('price', required=True, help='Item price should be provided')
 update_item_parser.add_argument('quantity', required=True, help='Item quantity should be provided')
-
-pagination_parser = reqparse.RequestParser()
-pagination_parser.add_argument('page', type='int', required=False, help='page number')
-pagination_parser.add_argument('bool', type='bool', required=False, help='page number')
-pagination_parser.add_argument('limit', type='int', required=False, help='limit per page',
-                               default=10)
-
-add_shopping_list = reqparse.RequestParser()
-add_shopping_list.add_argument('name', type='str', help='Name must be a string')
-add_shopping_list.add_argument('description', type=str, help='Description must be a string')
+update_item_parser.add_argument('unit', required=True, help='enter a unit of measurement')
